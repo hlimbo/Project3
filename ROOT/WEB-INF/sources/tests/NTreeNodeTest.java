e4482d02bf384be3fb9ee669c732ff19edac2b2c
ElectricSolstice <repairagent@yahoo.com>
2017-05-07 12:20:20 -0700

    Consumer test addded to NTreeNodeTest.

--- ROOT/WEB-INF/sources/tests/NTreeNodeTest.java
+++ ROOT/WEB-INF/sources/tests/NTreeNodeTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 import java.sql.*;
 import java.util.*;
+import java.util.function.*;
 
 import gamesite.datastruct.NTreeNode;
 
@@ -24,15 +25,54 @@ public class NTreeNodeTest
     }
 
 	@Test
-	public void testAddChild()
+	public void testAddAndRemoveChild()
 	{
         NTreeNode<Integer> node = new NTreeNode<Integer>(9);
-        node.addChild(new NTreeNode<Integer>(11));
-        node.addChild(new NTreeNode<Integer>(12));
-        node.addChild(new NTreeNode<Integer>(13));
+        NTreeNode<Integer> nodeOne = new NTreeNode<Integer>(11);
+        NTreeNode<Integer> nodeTwo = new NTreeNode<Integer>(12);
+        NTreeNode<Integer> nodeThree = new NTreeNode<Integer>(13);
+        node.addChild(nodeOne);
+        node.addChild(nodeTwo);
+        node.addChild(nodeThree);
         assertEquals("addChild not adding properly. Incorrect number of children.",new Integer(3),new Integer(node.children.size()));
         assertEquals("Data not initialized properly in addChild on 11",new Integer(11),node.children.get(0).data);
         assertEquals("Data not initialized properly in addChild on 12",new Integer(12),node.children.get(1).data);
         assertEquals("Data not initialized properly in addChild on 13",new Integer(13),node.children.get(2).data);
+        assertTrue("Failure removing nodeOne",node.removeChild(nodeOne));
+        assertTrue("Failure removing nodeTwo",node.removeChild(nodeTwo));
+        assertTrue("Failure removing nodeThree",node.removeChild(nodeThree));
+    }
+
+    private class StringConsumer<T> implements Consumer<T> {
+        public StringConsumer () {
+            result = "";
+        }
+        public void accept(T t) {
+            result+=t;
+            result+=",";
+        }
+        String result;
+    }
+
+	@Test
+	public void testConsumePreOrder()
+	{
+        NTreeNode<Integer> node = new NTreeNode<Integer>(0);
+        for (int i=0;i<10;++i) {
+            node.addChild(new NTreeNode<Integer>(i*10+1));
+        }
+        for (int i=0;i<10;++i) {
+            for (int j=1;j<10;++j) {
+                node.children.get(i).addChild(new NTreeNode<Integer>(i*10+1+j));
+            }
+        }
+        StringConsumer<Integer> result = new StringConsumer<Integer>();
+        String expected = "";
+        for (int i=0;i<101;++i) {
+            expected+=i;
+            expected+=",";
+        }
+        node.consumePreOrder(result);
+        assertEquals("Consumption failure",expected,result.result);
     }
 }
