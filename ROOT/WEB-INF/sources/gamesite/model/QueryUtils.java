6dfaacd0bdd52c35d3e82734f838775173a2fd5f
ElectricSolstice <repairagent@yahoo.com>
2017-05-06 18:08:20 -0700

    Untested but compilable SearchResults.java

--- ROOT/WEB-INF/sources/gamesite/model/QueryUtils.java
+++ ROOT/WEB-INF/sources/gamesite/model/QueryUtils.java
@@ -20,40 +20,43 @@ public class QueryUtils {
         return dbcon;
     }
 
+    public static String getValue (ResultSet result, ResultSetMetaData meta, int i) 
+        throws SQLException{
+        int type = meta.getColumnType(i);
+        String table = meta.getTableName(i);
+        String typeName = meta.getColumnTypeName(i);
+        boolean handled = false;
+        String value="";
+        switch(typeName.toUpperCase()) {
+            case "YEAR":
+                if (result.getString(i) != null) {
+                    value+=result.getString(i).substring(0,4);
+                    handled=true;
+                }
+                break;
+        }
+        if (!handled) {
+            switch(type) {
+                case Types.INTEGER:
+                    if (result.getString(i) != null) {
+                        value+=result.getInt(i);
+                    }
+                    break;
+                default:
+                    if (result.getString(i) != null) {
+                        value+=result.getString(i);
+                    }
+                    break;
+            }
+        }
+        return value;
+    }
+
 	public static HashMap<String, String> tableRow (ResultSet result) throws SQLException {
 		ResultSetMetaData meta = result.getMetaData();
         HashMap<String,String> row = new HashMap<String,String>();
 		for (int i=1;i<=meta.getColumnCount();++i) {
-			int type = meta.getColumnType(i);
-            String table = meta.getTableName(i);
-			String typeName = meta.getColumnTypeName(i);
-            boolean handled = false;
-            String value="";
-			switch(typeName.toUpperCase()) {
-			case "YEAR":
-                if (result.getString(i) != null) {
-				    value+=result.getString(i).substring(0,4);
-                    handled=true;
-                }
-				break;
-			}
-			if (!handled) {
-				switch(type) {
-				case Types.INTEGER:
-                    if (result.getString(i) != null) {
-					    value+=result.getInt(i);
-                    }
-					break;
-				default:
-                    if (result.getString(i) != null) {
-					    value+=result.getString(i);
-                    }
-					break;
-				}
-			}
-            //handle nulls and empty values here
-			String colName = meta.getColumnName(i);
-            row.put(colName,value);
+            row.put(meta.getColumnName(i),getValue(result,meta,i));
 		}
         return row;
 	}
--- ROOT/WEB-INF/sources/gamesite/model/SearchResults.java
+++ ROOT/WEB-INF/sources/gamesite/model/SearchResults.java
@@ -5,9 +5,26 @@ import java.net.*;
 import java.sql.*;
 import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+
+import gamesite.model.QueryUtils;
 
 public class SearchResults {
     private static String masterTable = "platforms_of_games NATURAL JOIN genres_of_games NATURAL JOIN publishers_of_games";
+
+    //Singleton class, so constructor not permitted outside of class
+    private SearchResults () {}
+
+    //Initialization-on-demand holder idiom
+    //https://en.wikipedia.org/wiki/Initialization-on-demand_holder_idiom
+    private static class SearchCache {
+        static final SearchResults INSTANCE = new SearchResults();
+    }
+    //TODO implement caching
+
+    public static SearchResults getInstance() {
+        return SearchCache.INSTANCE;
+    }
 
     private static String addSearchTerm (String value, String term, boolean useSubMatch) {
             String searchTerm = "";
@@ -27,8 +44,6 @@ public class SearchResults {
 
     private static int setSearchTerm (String value, String term, PreparedStatement statement, 
             int offset, boolean useSubMatch) throws SQLException {
-            Connection dbcon = QueryUtils.makeConn();
-            PreparedStatement statement; 
 
             String searchTerm = "";
             if (value != null && value.trim() != "") {
@@ -45,48 +60,123 @@ public class SearchResults {
             return offset;
     }
 
-    public ArrayList<HashMap<String,String>> search (String table, String limit, String offset,
-            String game, String year, String genre, String platform, String order, 
-            boolean descend, boolean match) throws SQLException, Java.lang.Exception {
-        ArrayList<HashMap<String,String>> table = new ArrayList<HashMap<String,String>> ();
-        String query;
+    private static ArrayList<HashMap<String,String>> callSearchQuery (String query, String table, 
+            String limit, String offset, String game, String year, String genre, 
+            String platform, String publisher, String order, 
+            boolean descend, boolean match) throws SQLException, java.lang.Exception {
+        ArrayList<HashMap<String,String>> results = new ArrayList<HashMap<String,String>> ();
+        Connection dbconn = null;
         try {
-            Connection dbconn=QueryUtils.createConn();
-            boolean searchAll = false;
-            if ((name==null || name.trim().compareTo("")==0) && (year==null || year.trim().compareTo("")==0)
-                    && (publisher==null || publisher.trim().compareTo("")==0) 
-                    && (genre==null || genre.trim().compareTo("")==0) 
-                    && (platform==null || platform.trim().compareTo("")==0)) {
-                searchAll = true;
-            }
-            if (!searchAll) {
-                //duplicates due to games on multiple platforms, with multiple genres, or etc...
-                query = "SELECT DISTINCT "+table+".* FROM games, publishers, platforms, genres, "+masterTable+" WHERE "
-                    +"games.id=game_id AND publishers.id=publisher_id AND platforms.id=platform_id";
-                query+=addSearchTerm(request,"name",useSubMatch);
-                query+=addSearchTerm(request,"year",useSubMatch);
-                query+=addSearchTerm(request,"publisher",useSubMatch);
-                query+=addSearchTerm(request,"genre",useSubMatch);
-                query+=addSearchTerm(request,"platform",useSubMatch);
-            } else {
-                query = "SELECT "+table+".* FROM "+table;
-            }
+            dbconn=QueryUtils.createConn();
+            PreparedStatement statement = dbconn.prepareStatement(query);
+            setSearchTerms(statement,game,year,publisher,genre,platform,match);
             ResultSet rs = statement.executeQuery();
-            rs.next();
+            while (rs.next()) {
+                results.add(QueryUtils.tableRow(rs));
+            }
         } catch (SQLException ex) {
             throw ex;
         } catch (java.lang.Exception ex) {
             throw ex;
         } finally {
-            conn.close();
+            if (dbconn != null) {
+                dbconn.close();
+            }
         }
+        return results;
     }
 
-    public int getCount () {
-        String query;
-        String originalQuery = query;
-        query = "SELECT COUNT(*) FROM ("+query+") AS countable";
-        int count = rs.getInt(1);
+    public static String buildQuery (String table, String limit, String offset,
+            String game, String year, String genre, String platform, String publisher, 
+            String order, boolean descend, 
+            boolean match) throws SQLException, java.lang.Exception {
+        String query="";
+        boolean searchAll = false;
+        if ((game==null || game.trim().compareTo("")==0) && (year==null || year.trim().compareTo("")==0)
+                && (publisher==null || publisher.trim().compareTo("")==0) 
+                && (genre==null || genre.trim().compareTo("")==0) 
+                && (platform==null || platform.trim().compareTo("")==0)) {
+            searchAll = true;
+        }
+        if (!searchAll) {
+            //duplicates due to games on multiple platforms, with multiple genres, or etc...
+            query = "SELECT DISTINCT "+table+".* FROM games, publishers, platforms, genres, "+masterTable+" WHERE "
+                +"games.id=game_id AND publishers.id=publisher_id AND platforms.id=platform_id";
+            query+=addSearchTerm(game,"name",match);
+            query+=addSearchTerm(year,"year",match);
+            query+=addSearchTerm(publisher,"publisher",match);
+            query+=addSearchTerm(genre,"genre",match);
+            query+=addSearchTerm(platform,"platform",match);
+        } else {
+            query = "SELECT "+table+".* FROM "+table;
+        }
+        return query;
     }
 
+    private static void setSearchTerms (PreparedStatement statement,String name, String year, String publisher,
+            String genre, String platform,boolean useSubMatch) throws SQLException {
+        int statementOffset = 1;
+        statementOffset = setSearchTerm(name,"name",statement,statementOffset,useSubMatch);
+        statementOffset = setSearchTerm(year,"year",statement,statementOffset,useSubMatch);
+        statementOffset = setSearchTerm(publisher,"publisher",statement,statementOffset,useSubMatch);
+        statementOffset = setSearchTerm(genre,"genre",statement,statementOffset,useSubMatch);
+        setSearchTerm(platform,"platform",statement,statementOffset,useSubMatch);
+    }
+
+    public ArrayList<HashMap<String,String>> search (String table, String limit, String offset,
+            String game, String year, String genre, String platform, String publisher, String order, 
+            boolean descend, boolean match) throws SQLException, java.lang.Exception {
+        String query = buildQuery(table,limit,offset,game,year,genre,platform,publisher,order,descend,match);
+        if (descend) {
+            query+=" ORDER BY ISNULL( "+table+"."+order+"), "+table+"."+order+" DESC, "
+                +table+".id LIMIT "+limit+" OFFSET "+offset;
+        } else {
+            query+=" ORDER BY ISNULL( "+table+"."+order+"), "+table+"."+order+" ASC, "
+                +table+".id LIMIT "+limit+" OFFSET "+offset;
+        }
+        ArrayList<HashMap<String,String>> results = cache.get(query);
+        if (results == null) {
+            results = callSearchQuery(query,
+                table,limit,offset,game,year,genre,
+                platform,publisher,order,descend,match);
+            if (results != null) {
+                return cache.putIfAbsent(query,results);
+            }
+            /*return cache.computeIfAbsent(query, qry -> {
+                try {
+                    return callSearchQuery(qry,
+                        table,limit,offset,game,year,genre,
+                        platform,publisher,order,descend,match);
+                } catch (SQLException ex) {
+                    throw ex;
+                }
+                    });*/
+        }
+        return results;
+    }
+
+    public int getCount(String table, String limit, String offset,
+            String game, String year, String genre, String platform, String publisher,
+            String order, boolean descend, boolean match) throws SQLException, java.lang.Exception {
+        String countQuery = "SELECT COUNT(*) FROM ("+
+            buildQuery(table,limit,offset,game,year,genre,platform,publisher,order,descend,match)
+            +") AS countable";
+        int count = -1;
+        Connection dbconn=null;
+        try {
+            dbconn=QueryUtils.createConn();
+            PreparedStatement statement = dbconn.prepareStatement(countQuery);
+            setSearchTerms(statement,game,year,publisher,genre,platform,match);
+            ResultSet rs = statement.executeQuery();
+            rs.next();
+            count = rs.getInt(1);
+        } finally {
+            if (dbconn!=null) {
+                dbconn.close();
+            }
+        }
+        return count;
+    }
+
+    private ConcurrentMap<String,ArrayList<HashMap<String,String>>> cache;
 }
