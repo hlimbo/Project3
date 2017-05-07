cb6cc84e0be82292cd9393cde33d4a6c9ce81d92
ElectricSolstice <repairagent@yahoo.com>
2017-05-07 15:49:12 -0700

    Planning out Table object.

--- ROOT/WEB-INF/sources/gamesite/datastruct/Table.java
+++ ROOT/WEB-INF/sources/gamesite/datastruct/Table.java
@@ -4,11 +4,17 @@ import java.util.*;
 
 public class Table {
 
-    public Table (String newName) {
+    public Table (String newName,String keyName) {
         name = newName;
-        rows = new HashMap<String,String>();
+        rows = new HashMap<String,HashMap<String,String>>();
+        key = keyName;
     }
 
-    public HashMap<String,String> rows;
+    //String key in outer HashMap is a primary key to find a particular
+    //row by.
+    public HashMap<String,HashMap<String,String>> rows;
     public final String name;
+    //primary key of table for quick searching of particular
+    //row
+    public final String key;
 }
