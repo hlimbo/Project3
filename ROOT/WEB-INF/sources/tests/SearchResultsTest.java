ee12565f6ba4a311382185471f950f880935b26e
ElectricSolstice <repairagent@yahoo.com>
2017-05-07 19:47:19 -0700

    getCount tested now

--- ROOT/WEB-INF/sources/tests/SearchResultsTest.java
+++ ROOT/WEB-INF/sources/tests/SearchResultsTest.java
@@ -14,7 +14,8 @@ import java.util.*;
 import gamesite.model.*;
 
 public class SearchResultsTest {
-    @Test public void searchTest () {
+    @Test
+    public void searchTest () {
         ArrayList<HashMap<String, String>> table;
         try {
             table = SearchResults.getInstance().search("publishers","1",
@@ -27,6 +28,20 @@ public class SearchResultsTest {
             assertNotNull(table);
             assertFalse(table.isEmpty());
             assertEquals(table.get(0).get("name"),"Wii Sports");
+            assertNotEquals(table.get(0).get("name"),"TESTABLE_INCORRECT_VALUE");
+        } catch (SQLExceptionHandler ex) {
+            System.out.println(ex.getErrorMessage());
+        } catch (java.lang.Exception e) {
+			e.printStackTrace();
+		}
+    }
+    @Test
+    public void getCountTest () {
+        Integer count; 
+        try {
+            count = SearchResults.getInstance().getCount("publishers","1",
+                    "0","","","","","Nintendo","id",false,1);
+            assertEquals((long)count,(long)1);
         } catch (SQLExceptionHandler ex) {
             System.out.println(ex.getErrorMessage());
         } catch (java.lang.Exception e) {
