b662417ca9212da031d2f354ea54fe86d840cd80
ElectricSolstice <repairagent@yahoo.com>
2017-05-08 21:53:55 -0700

    Sketching out how DashBoard will work.

---
+++ ROOT/WEB-INF/sources/gamesite/model/DashBoardCommands.java
@@ -0,0 +1,21 @@
+package gamesite.model;
+
+public class DashBoardCommands {
+    //Error messages returned as strings.
+    //Return null on success
+
+    public String addMovie () {
+        String commandResult = null;
+        return commandResult;
+    }
+
+    public String getMeta () {
+        String commandResult = null;
+        return commandResult;
+    }
+
+    public String insertStar () {
+        String commandResult = null;
+        return commandResult;
+    }
+}
--- ROOT/WEB-INF/sources/gamesite/servlet/DashBoardServlet.java
+++ ROOT/WEB-INF/sources/gamesite/servlet/DashBoardServlet.java
@@ -8,14 +8,16 @@ import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class SearchServlet extends HttpServlet {
+public class DashBoardServlet extends HttpServlet {
 
     public String getServletInfo() {
-        return "Servlet connects to MySQL database and displays result of a SELECT";
+        return "Servlet for employee interface";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
+        //TODO Parse parameters and then send to another class to handle
+        //the processing of the commands
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response)
