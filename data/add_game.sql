95ca9ed17d90695c4c02a49a8ecefe8e4457cd58
ElectricSolstice <repairagent@yahoo.com>
2017-05-08 21:10:22 -0700

    add_game.sql seems to be working

--- data/add_game.sql
+++ data/add_game.sql
@@ -1,6 +1,6 @@
 DELIMITER //
 
-CREATE PROCEDURE add_game (IN newName VARCHAR(50), IN newYear VARCHAR(50), IN newPrice VARCHAR(50), IN newPlatform VARCHAR(50), IN newPublisher VARCHAR(50), IN newGenre VARCHAR(50))
+CREATE PROCEDURE add_game (IN newName VARCHAR(200), IN newYear YEAR, IN newPrice INTEGER, IN newPlatform VARCHAR(200), IN newPublisher VARCHAR(200), IN newGenre VARCHAR(200))
 BEGIN
     DECLARE countFound INTEGER;
     DECLARE platformID INTEGER;
@@ -23,12 +23,21 @@ BEGIN
     END IF;
     SET countFound = (SELECT COUNT(*) FROM genres WHERE genre = newGenre);
     IF countFound = 0 THEN 
-        INSERT INTO genre (genre) VALUES (newGenre);
+        INSERT INTO genres (genre) VALUES (newGenre);
     END IF;
-    SET genreID = (SELECT id FROM genre WHERE genre = newGenre);
+    SET genreID = (SELECT id FROM genres WHERE genre = newGenre);
     SET countFound = (SELECT COUNT(*) FROM genres_of_games WHERE genre_id = genreID AND game_id = gameID);
     IF countFound = 0 THEN 
         INSERT INTO genres_of_games (game_id, genre_id) VALUES (gameID, genreID);
+    END IF;
+    SET countFound = (SELECT COUNT(*) FROM publishers WHERE publisher = newPublisher);
+    IF countFound = 0 THEN 
+        INSERT INTO publishers (publisher) VALUES (newPublisher);
+    END IF;
+    SET publisherID = (SELECT id FROM publishers WHERE publisher = newPublisher);
+    SET countFound = (SELECT COUNT(*) FROM publishers_of_games WHERE publisher_id = publisherID AND platform_id = platformID AND game_id = gameID);
+    IF countFound = 0 THEN 
+        INSERT INTO publishers_of_games (publisher_id, platform_id, game_id) VALUES (publisherID,platformID,gameID);
     END IF;
 END
 //
