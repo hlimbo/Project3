7013506e537b5276244655a526c5f22507873d8d
ElectricSolstice <repairagent@yahoo.com>
2017-05-08 23:27:59 -0700

    getMeta implemented in DashBoardCommands.java

--- ROOT/WEB-INF/sources/gamesite/model/DashBoardCommands.java
+++ ROOT/WEB-INF/sources/gamesite/model/DashBoardCommands.java
@@ -1,17 +1,35 @@
 package gamesite.model;
 
+import java.sql.*;
+import java.util.*;
+
+import gamesite.model.QueryUtils;
+import gamesite.utils.DBConnection;
+
 public class DashBoardCommands {
-    //Error messages returned as strings.
-    //Return null on success
 
     public String addMovie () {
         String commandResult = null;
         return commandResult;
     }
 
-    public String getMeta () {
-        String commandResult = null;
-        return commandResult;
+    public LinkedHashMap<String,HashMap<String,String>> getMeta () throws SQLException, java.lang.Exception {
+        LinkedHashMap<String,HashMap<String,String>> types = new LinkedHashMap<String,HashMap<String,String>>();
+        Connection conn = null;
+        try {
+            conn = DBConnection.create();
+            ArrayList<String> tables = QueryUtils.getTables(conn);
+            for (String table : tables) {
+                types.put(table,QueryUtils.getColumns(conn,table));
+            }
+        } catch(SQLException ex) {
+            throw ex;
+        } catch(java.lang.Exception ex) {
+            throw ex;
+        }finally {
+            DBConnection.close(conn);
+        }
+        return types;
     }
 
     public String insertStar () {
