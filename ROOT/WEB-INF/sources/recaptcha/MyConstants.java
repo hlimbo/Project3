4abaa5f908cefd09d93b3552435b2279625108b0
hlimbo <limboh27@gmail.com>
2017-05-09 19:46:23 -0700

    Setup recaptcha java files and web.xml

---
+++ ROOT/WEB-INF/sources/recaptcha/MyConstants.java
@@ -0,0 +1,8 @@
+package recaptcha;
+
+class MyConstants 
+{
+    public static final String SITE_KEY ="6LeuvCAUAAAAAB68t-UTWj7SyL02eIYRd3MOswzm";
+    public static final String SECRET_KEY ="6LeuvCAUAAAAAEoMMF7n8IJFDTCilJpyRIP1V5DW";
+}
+
