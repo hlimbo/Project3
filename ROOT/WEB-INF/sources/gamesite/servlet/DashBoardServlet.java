72e9ea126a014fe12c5c046d9aa259db18a89bc9
ElectricSolstice <repairagent@yahoo.com>
2017-05-09 22:01:10 -0700

    Exception formatters compilable

--- ROOT/WEB-INF/sources/gamesite/servlet/DashBoardServlet.java
+++ ROOT/WEB-INF/sources/gamesite/servlet/DashBoardServlet.java
@@ -8,6 +8,9 @@ import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import gamesite.utils.*;
+import gamesite.model.DashBoardCommands;
+
 public class DashBoardServlet extends HttpServlet {
 
     public String getServletInfo() {
@@ -18,6 +21,37 @@ public class DashBoardServlet extends HttpServlet {
         throws IOException, ServletException {
         //TODO Parse parameters and then send to another class to handle
         //the processing of the commands
+        HashMap<String,String> params = ParameterParse.getQueryParameters(request.getQueryString());
+        if (params.containsKey("command") {
+            String command = params.get("command");
+            PrintWriter writer=null;
+            try {
+                PrintWriter writer = response.getWriter();
+                switch (command) {
+                    case "add_game":
+                        DashBoardCommands.addGame(params.get("name"),
+                                params.get("year"),params.get("price"),params.get("platform"),
+                                params.get("publisher"),params.get("genre"));
+                        break;
+                    case "insert_publisher":
+                        DashBoardCommands.insertPublisher(params.get("publisher"));
+                        break;
+                    case "meta":
+                        DashBoardCommands.getMeta();
+                        break;
+                    default:
+                        break;
+                }
+            } catch (SQLExceptionHandler ex) {
+                writer.println(SQLExceptionFormat.toHtml(ex));
+            } catch (SQLException ex) {
+                writer.println(SQLExceptionFormat.toHtml(ex));
+            } catch (java.lang.Exception ex)  {
+                writer.println(ExceptionFormat.toHtml(ex));
+            } finally {
+                writer.close();
+            }
+        }
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response)
--- ROOT/WEB-INF/sources/gamesite/utils/SQLExceptionFormat.java
+++ ROOT/WEB-INF/sources/gamesite/utils/SQLExceptionFormat.java
@@ -1,30 +1,14 @@
+package gamesite.utils;
+
 import java.sql.*;
 import java.io.*;
 
 import gamesite.model.SQLExceptionHandler;
+import gamesite.utils.ExceptionFormat;
 
-class SQLExceptionFormat {
+class SQLExceptionFormat extends ExceptionFormat {
     //Ensures static class
-    private SQLExceptionFormat () {}
-
-    public static String htmlHeader () {
-        return ("<HTML>\n<HEAD><TITLE>\ngamedb: Error"
-            +"</TITLE>\n</HEAD>\n<BODY>\n" +
-                    "<P>");
-    }
-
-    public static String htmlFooter () {
-            return "</P></BODY></HTML>";
-    }
-
-    public static String getTrace (SQLException ex) {
-        StackTraceElement [] trace = ex.getStackTrace();
-        String traceString = "";
-        for (StackTraceElement stackTop : trace) {
-            traceString+=stackTop.toString();
-        }
-        return traceString;
-    }
+    protected SQLExceptionFormat () {}
 
     public static String toHtml (SQLExceptionHandler ex) {
         return htmlHeader()+"\n"+ex.getErrorMessage()+getTrace(ex)+"\n"+htmlFooter();
