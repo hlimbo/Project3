c0932ec0dea22cd598f6e4eeda226c7d5a2eab90
hlimbo <limboh27@gmail.com>
2017-05-14 16:31:22 -0700

    Add PublishersParser

--- XML_Parsing/sources/PublishersParser.java
+++ XML_Parsing/sources/PublishersParser.java
@@ -1,4 +1,5 @@
-import model.pub;
+import model.Pub;
+import model.Gplt;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,9 +17,24 @@ import org.xml.sax.helpers.DefaultHandler;
 
 public class PublishersParser extends DefaultHandler
 {
+	public static final String PUB = "pub";
+	public static final String P_ID = "p_id";
+	public static final String P = "p";
+	public static final String FND = "fnd";
+	public static final String GPLT = "gplt";
+	public static final String G = "g";
+	public static final String PLT = "plt";
+	
+	private List<Pub> publishers;
+	
+	//to maintain context
+	private Pub tempPub;
+	private Gplt tempRecord;
+	private String tempVal;
+	
 	public PublishersParser()
 	{
-		
+		publishers = new ArrayList<Pub>();
 	}
 	
 	public void parseDocument(String filename)
@@ -42,22 +58,65 @@ public class PublishersParser extends DefaultHandler
         }
 	}
 	
+	public void printData()
+	{	
+		System.out.println("Publishers Size: " + publishers.size());
+		Iterator<Pub> it = publishers.iterator();
+		while(it.hasNext())
+			System.out.println(it.next().toString());
+	}
+	
 	//Event Handlers
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException 
 	{
+		if(qName.equalsIgnoreCase(PUB))
+			tempPub = new Pub();
+		else if(qName.equalsIgnoreCase(GPLT))
+		{	//initialize a platform game list on the first gplt tag encountered within a <pub> tag
+			if(tempPub != null && tempPub.isPlatformGameListNull())
+				tempPub.initializePlatformGameList();
+			
+			//intialize a gplt record to store into the platform game list
+			tempRecord = new Gplt();
+		}
+		
 	}
 	
 	@Override
 	public void characters(char[] ch, int start, int length) throws SAXException
 	{
-		
+		tempVal = new String(ch, start, length);
 	}
 	
 	@Override
     public void endElement(String uri, String localName, String qName) throws SAXException
 	{
-		
+			if(qName.equalsIgnoreCase(PUB))
+				publishers.add(tempPub);
+			else if(qName.equalsIgnoreCase(P_ID))
+				tempPub.setPubID(tempVal);
+			else if(qName.equalsIgnoreCase(P))
+				tempPub.setPublisher(tempVal);
+			else if(qName.equalsIgnoreCase(FND))
+				tempPub.setFoundedYear(tempVal);
+			else if(qName.equalsIgnoreCase(GPLT))
+				tempPub.addGamePlatformRecord(tempRecord);
+			else if(qName.equalsIgnoreCase(G))
+				tempRecord.gameTitle = tempVal;
+			else if(qName.equalsIgnoreCase(PLT))
+				tempRecord.platform = tempVal;				
 	}	
+	
+	public static void main(String[] args)
+	{
+		PublishersParser pr = new PublishersParser();
+		long startTime = System.currentTimeMillis();
+		pr.parseDocument("newGames/pubs.xml");
+		long endTime = System.currentTimeMillis();	
+		long elapsedTime = endTime - startTime;	
+		pr.printData();
+		System.out.println("Execution parse time: " + elapsedTime + " ms");
+	}
 	
 }
---
+++ XML_Parsing/sources/model/Gplt.java
@@ -0,0 +1,22 @@
+package model;
+
+
+//platform of games
+public class Gplt
+{
+	public String gameTitle;//<g>
+	public String platform;//<plt>
+	
+	public Gplt() {}
+	public Gplt(String gameTitle, String platform)
+	{
+		this.gameTitle = gameTitle;
+		this.platform = platform;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "[ gameTitle: " + gameTitle +  ", platform: " + platform + " ]";
+	}
+}
