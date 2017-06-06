cc4a71bbb16d971062448be9c56bcc6e15e720cf
ElectricSolstice <repairagent@yahoo.com>
2017-06-06 15:50:20 -0700

    Connection pooling added to DBConnection

--- ROOT/WEB-INF/sources/gamesite/utils/DBConnection.java
+++ ROOT/WEB-INF/sources/gamesite/utils/DBConnection.java
@@ -1,6 +1,9 @@
 package gamesite.utils;
 
 import java.sql.*;
+import javax.sql.DataSource;
+import javax.naming.InitialContext;
+import javax.naming.Context;
 
 /*
 	DBConnection is a static class responsible for 2 things:
@@ -12,15 +15,22 @@ public class DBConnection
 {
 	public static Connection create() throws SQLException, java.lang.Exception
 	{
-		//since we have jdbc 5.0, we don't need to create a new instance
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
 		
-		String user = "user";
+		/*String user = "user";
 		String password = "password";
 		//disable and suppress SSL errors; if SSL=true, it is required to verify that the database connection is secure
 		String loginUrl = "jdbc:mysql://localhost:3306/gamedb?useSSL=false";
 		
-		return DriverManager.getConnection(loginUrl, user, password);
+		return DriverManager.getConnection(loginUrl, user, password);*/
+
+        //Update to use connection pooling
+        Context initCtx = new InitialContext();
+
+        Context envCtx = (Context) initCtx.lookup("java:comp/env");
+        DataSource ds = (DataSource) envCtx.lookup("jdbc/GameDB");
+
+        return ds.getConnection();
 	}
 	
 	public static void close(Connection dbcon) throws SQLException
