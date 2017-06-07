40c45bcb1bc127ff2f64551b297d7cd03f3a278b
ElectricSolstice <repairagent@yahoo.com>
2017-06-07 14:35:42 -0700

    DBConnection method added to allow slave to receive reads from either tomcat

--- ROOT/WEB-INF/sources/gamesite/utils/DBConnection.java
+++ ROOT/WEB-INF/sources/gamesite/utils/DBConnection.java
@@ -13,6 +13,8 @@ import javax.naming.Context;
 
 public class DBConnection
 {
+    protected static int connectionNumber = 0;
+
 	public static Connection create() throws SQLException, java.lang.Exception
 	{
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
@@ -28,7 +30,33 @@ public class DBConnection
         Context initCtx = new InitialContext();
 
         Context envCtx = (Context) initCtx.lookup("java:comp/env");
-        DataSource ds = (DataSource) envCtx.lookup("jdbc/GameDB");
+        DataSource ds = (DataSource) envCtx.lookup("jdbc/GameReadWrite");
+
+        return ds.getConnection();
+	}
+
+	public static Connection createRead() throws SQLException, java.lang.Exception
+	{
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		
+		/*String user = "user";
+		String password = "password";
+		//disable and suppress SSL errors; if SSL=true, it is required to verify that the database connection is secure
+		String loginUrl = "jdbc:mysql://localhost:3306/gamedb?useSSL=false";
+		
+		return DriverManager.getConnection(loginUrl, user, password);*/
+
+        //Update to use connection pooling
+        Context initCtx = new InitialContext();
+
+        Context envCtx = (Context) initCtx.lookup("java:comp/env");
+        DataSource ds = null;
+        if (connectionNumber % 2 ==0) {
+            ds = (DataSource) envCtx.lookup("jdbc/GameRead");
+        } else {
+            ds = (DataSource) envCtx.lookup("jdbc/GameReadWrite");
+        }
+        ++connectionNumber;
 
         return ds.getConnection();
 	}
