7f4b01b122b2aa6f6a7c130573f17d45d48b6c78
ElectricSolstice <repairagent@yahoo.com>
2017-06-08 15:18:40 -0700

    All jmx files needed for jmeter testing

--- ROOT/meter.sh
+++ ROOT/meter.sh
@@ -1,9 +1,10 @@
 rm -rf singlesingle/
 rm -f singlesingle.txt
-jmeter -n -t SingleThreadSingleInstance.jmx -l singlesingle.txt -e -o singlesingle/
+#jmeter -n -t SingleThreadSingleInstance.jmx -l singlesingle.txt -e -o singlesingle/
+jmeter -n -t SingleThreadSingleInstance.jmx -e -o singlesingle/
 rm -rf tensingle/
 rm -f tensingle.txt
-jmeter -n -t TenThreadSingleInstance.jmx -l tensingle.txt -e -o tensingle/
-#jmeter -n -t HttpsTenThreadSingleInstance.jmx -l httpstensingle.txt
-#jmeter -n -t SingleThreadCluster.jmx -l singlecluster.txt
-#jmeter -n -t TenThreadCluster.jmx -l tencluster.txt
+jmeter -n -t TenThreadSingleInstance.jmx -e -o tensingle/
+jmeter -n -t HttpsTenThreadSingleInstance.jmx -l httpstensingle.txt
+jmeter -n -t SingleThreadCluster.jmx -l singlecluster.txt
+jmeter -n -t TenThreadCluster.jmx -l tencluster.txt
